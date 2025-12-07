14292e50c42cf45afe2af15f4b51d17295b84671
muhammad-thariq <mthariqd@gmail.com>
2025-12-07 18:38:08 +0700

    Fix build error: populate profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Profile Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
